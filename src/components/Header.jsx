bb1843697be17c4c482d89dc9e190111f1e41253
VENKATA_REDDY <venkatareddypadala1637@gmail.com>
2025-02-02 12:19:38 +0530

    svg path changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
-// import reactLogo from "@/assets/react.svg";
 function Header() {
   return (
     <header className="app-header">
-      <img src="../assets/react.svg" alt="React logo" />
+      <img src="/React-Quiz/react.svg" alt="React logo" />
       <h1>The React Quiz App</h1>
     </header>
   );
